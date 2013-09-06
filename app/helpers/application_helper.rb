9f7b585854d899add3956c332746363b6b54db62
Ruby-Rails <ruby@zuplo.net>
2013-09-06 10:52:43 +0100

    Finish layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-
+  # Supports: spec/requests/static_pages_spec.rb
   # Returns the full title on a per-page basis.       # Documentation comment
   def full_title(page_title)                          # Method definition
     base_title = "Ruby on Rails Tutorial Sample App"  # Variable assignment
